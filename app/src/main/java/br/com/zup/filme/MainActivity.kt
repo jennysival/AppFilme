da5c49bf0cd4576e6a19069ae5289bd2ae93726e
jennysival <jeniffersival@gmail.com>
2022-05-25 15:04:53 -0300

    Adicionar botão com toast

--- app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.zup.filme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoMensagem = findViewById<Button>(R.id.botao_mensagem)
+
+        botaoMensagem.setOnClickListener {
+            Toast.makeText(this, "Esse filme é maravilhoso", Toast.LENGTH_LONG).show()
+        }
     }
 }
